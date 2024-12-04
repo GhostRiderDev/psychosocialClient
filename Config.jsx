1a7aa2c9545d47c9121b9e98497fe36bed59c990
cos4h <olvadis2004@gmail.com>
2024-12-03 22:01:27 -0500

    fixed path

--- Config.jsx
+++ Config.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 const baseAxios = axios.create({
-  baseURL: `${import.meta.env.VITE_BASE_URL}/api/v1`,
+  baseURL: `${import.meta.env.VITE_BASE_URL}api/v1`,
   timeout: 10000,
   headers: { "X-Custom-Header": "foobar", Authorization: `Bearer ${token}` },
 });
